3461fe6e8bdd3ee540b62859b17f180e271c8cd4
Ayushi Soni <ayushisoni@Ayushis-MacBook-Air.local>
2024-10-27 04:28:57 +0530

    Added ready event to emit new user joining for making calls on frontend

--- src/Pages/Room.tsx
+++ src/Pages/Room.tsx
@@ -5,19 +5,30 @@ import UserFeedPlayer from "../Components/UserFeedPlayer";
 
 const Room: React.FC = () => {
     const { id } = useParams();
-    const { socket, user, stream } = useContext(SocketContext);
+    const { socket, user, stream, peers } = useContext(SocketContext);
 
     useEffect(()=>{
         //emitting this event so that either creator of room or joinee in the room
         //anyone is added the server knows that new people have been added 
         //to this room
         if(user) socket.emit("joined-room", {roomId: id, peerId: user._id});
-    }, [id, user, socket]);
+        console.log(peers);
+    }, [id, user, socket, peers]);
 
     return (
         <div>
             room: {id}
+            <br/>
+            Youd own user feed
             <UserFeedPlayer stream={stream}/>
+            <div>
+                Other user's feed
+                {Object.keys(peers).map((peerId)=>(
+                    <>
+                        <UserFeedPlayer key={peerId} stream={peers[peerId].stream}/>
+                    </>
+                ))}
+            </div>
         </div>
     )
 }
